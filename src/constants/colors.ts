2833efd44647fa1df79e07008f7ecf4a836be684
Shakib Ahmed <shakibshovon.10@gmail.com>
2025-03-26 14:01:42 +0600

    addded intro screen

---
+++ src/constants/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  white: '#fff',
+  lightGray: 'rgba(231, 231, 231, 0.9)',
+  gray: 'rgba(128, 128, 128, 0.3)',
+  blackShadow: 'rgba(0, 0, 0, 0.5)',
+  lightBlackShadow: 'rgba(0, 0, 0, 0.4)',
+};
---
+++ src/utils/typography.ts
@@ -0,0 +1,6 @@
+export const fontFamily = {
+  light: 'Ubuntu-Light',
+  regular: 'Ubuntu-Regular',
+  medium: 'Ubuntu-Medium',
+  bold: 'Ubuntu-Bold',
+};
