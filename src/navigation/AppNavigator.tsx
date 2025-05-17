ec4882227afd2d296ac597f349ae307f0812eaca
Shakib Ahmed <shakibshovon.10@gmail.com>
2025-05-17 16:08:27 +0600

    removed options from profilescreen navigation

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,14 +1,9 @@
-import {
-  createNativeStackNavigator,
-  NativeStackNavigationOptions,
-} from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import CoffeeDetailScreen from '../screens/Coffee/CoffeeDetailScreen';
 
-import {colors} from '../constants/colors';
 import ProfileScreen from '../screens/Coffee/ProfileScreen';
 import {RootStackParamList} from '../types/types';
-import {fontFamily} from '../utils/typography';
 import BottomTabNavigator from './BottomTabNavigator';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -18,27 +13,7 @@ const AppNavigator = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="MainTabs" component={BottomTabNavigator} />
       <Stack.Screen name="CoffeeDetailScreen" component={CoffeeDetailScreen} />
-      <Stack.Screen
-        name="ProfileScreen"
-        component={ProfileScreen}
-        options={() =>
-          ({
-            headerShown: false,
-            headerTitle: '',
-            headerTitleAlign: 'center',
-            headerShadowVisible: false,
-            headerStyle: {
-              backgroundColor: colors.background,
-            },
-            headerTitleStyle: {
-              color: colors.white,
-              fontFamily: fontFamily.medium,
-            },
-            headerTintColor: colors.white,
-            headerLeft: () => '',
-          } as NativeStackNavigationOptions)
-        }
-      />
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
     </Stack.Navigator>
   );
 };
